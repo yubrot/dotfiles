6075496882d8a9870e87d10eb64e201a1b344375
yubrot <yubrot@gmail.com>
2026-01-27 10:33:25 +0900

    Update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,7 +18,7 @@ config.audible_bell = 'Disabled'
 config.visual_bell = { fade_out_duration_ms = 50 }
 config.enable_scroll_bar = true
 config.colors = { visual_bell = '#333333' }
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'UDEV Gothic NFLG'
 config.font_size = 16
 config.window_frame = { font_size = 16 }
 config.send_composed_key_when_left_alt_is_pressed = true
